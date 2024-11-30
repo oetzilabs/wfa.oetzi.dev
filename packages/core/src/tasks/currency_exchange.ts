29d1d01e79652dc7940130a5880de03c78c92aba
Özgür Isbert <oezguerisbert@gmail.com>
2024-11-30 15:30:49 +0100

    feat: currency exchange

---
+++ packages/core/src/tasks/currency_exchange.ts
@@ -0,0 +1,88 @@
+import {
+  array,
+  date,
+  entriesFromList,
+  literal,
+  number,
+  object,
+  picklist,
+  record,
+  strictObject,
+  string,
+  variant,
+} from "valibot";
+import { TaskGenerator } from "./generator";
+
+const currencies = picklist(["eur", "usd", "chf", "gbp"]);
+
+const [schema, run] = TaskGenerator.create({
+  input: variant("date", [
+    strictObject({
+      date: date(),
+      from: currencies,
+      to: array(currencies),
+      value: number(),
+    }),
+    strictObject({
+      date: literal("latest"),
+      from: currencies,
+      to: array(currencies),
+      value: number(),
+    }),
+  ]),
+  output: strictObject({
+    values: record(currencies, number()),
+  }),
+  fn: async (input) => {
+    let date: string = "latest";
+    let dateFallback: string = "latest";
+    if (input.date !== "latest") {
+      date = `${input.date.getFullYear()}.${input.date.getMonth() + 1}.${input.date.getDate()}`;
+      dateFallback = `${input.date.getFullYear()}-${input.date.getMonth() + 1}-${input.date.getDate()}`;
+    }
+    console.log(`Fetching data for ${input.from} to (${input.to.join(", ")}) on ${date}`);
+    const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${date}/v1/currencies/${input.from.toLowerCase()}.json`;
+    const fallbackUrl = `https://${dateFallback}.currency-api.pages.dev/v1/currencies/${input.from.toLowerCase()}.json`;
+    let response = await fetch(fallbackUrl);
+    if (!response.ok) {
+      response = await fetch(url);
+    }
+    if (!response.ok) {
+      throw new Error("Could not fetch data");
+    }
+    const data = (await response.json()) as {
+      date: string;
+    } & { [name: string]: Record<string, number> };
+    if (!data) {
+      throw new Error("Could not fetch data");
+    }
+    const fromCheck = Object.hasOwn(data, input.from.toLowerCase());
+    if (!fromCheck) {
+      throw new Error("Could not find from currency");
+    }
+    const from = data[input.from.toLowerCase()];
+
+    const result: Record<(typeof input.to)[number], number> = Object.create({});
+    for (const to of input.to) {
+      const toCheck = Object.hasOwn(from, to.toLowerCase());
+      if (!toCheck) {
+        throw new Error("Could not find to currency");
+      }
+      result[to] = from[to.toLowerCase()] * input.value;
+    }
+    result[input.from] = input.value;
+
+    return {
+      values: result,
+    };
+  },
+});
+
+const result = await run({
+  date: "latest",
+  from: "eur",
+  to: ["usd", "chf"],
+  value: 100,
+});
+
+result.type === "success" ? console.log(result.data.values) : console.log(result.error);
